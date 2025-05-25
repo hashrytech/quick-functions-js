97d96a5ce53734389ecf9bd2df4cb03c6e4da118
Mat70x7 <mmoxam@hotmail.com>
2025-05-25 17:21:33 -0500

    initial install

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # quick-functions-js
 A collection of useful functions written in Type Script
+
+## Install
+
+You can install the library via npm.
+
+```bash
+npm install @hashrytech/quick-functions-js --save
+```
+
+or via yarn:
+
+```bash
+yarn add @hashrytech/quick-functions-js
+```
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'Quick-Functions-JS',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `quick-functions-js.${format === 'es' ? 'mjs' : 'cjs'}`,
+    },
+    rollupOptions: {
+      external: [], // Add dependencies here if you have any
+    },
+  },
+  plugins: [dts({ outDir: 'dist' })],
+});
